a1d8686038703f2aa11693dba4819e5badc77c6d
hayoung <hayoung1214@naver.com>
2021-09-20 15:00:37 +0900

    feat : jpa mysql 연결완료

--- src/main/java/com/springboard/repository/MemberRepository.java
+++ src/main/java/com/springboard/repository/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.springboard.repository;
 
-public class MemberRepository {
+import com.springboard.dto.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByIdAAndPwd(String id, String pwd);
+    Optional<Member> findById(String id);
 }
--- src/main/java/com/springboard/service/MemberService.java
+++ src/main/java/com/springboard/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.springboard.service;
+
 import com.springboard.dto.Member;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 public class MemberService {
+
 }
